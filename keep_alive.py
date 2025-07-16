df6a375e6a8da86322058ee388854e1cfb5ef202
sov4414 <mafumafu0210@gmail.com>
2025-07-16 21:28:19 +0900

    Add files via upload

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,7 @@ def home():
     return "I am alive!"
 
 def run():
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
